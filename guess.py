0369f34b69abf719c7b6c644e595aa9d4a54d732
JpatrickR <patrickramirez886@gmail.com>
2021-05-01 17:22:58 +0800

    Added function Easy, Medium, Hard

---
+++ guess.py
@@ -0,0 +1,47 @@
+# Guessing Game
+import random
+
+# Guess Game
+
+
+def guess_game(limit, number):
+     random_number = random.randint(1, number)
+    try:
+        while limit > 0:
+            guess = int(input('What is your guess'))
+            limit -= 1
+            if random_number == guess:
+                print('You got it right!')
+                break
+            elif guess > number:
+                print('Guess is out of range')
+                print(f'You have {limit} guess(es) left')
+            else:
+                print('That was wrong')
+                print(f'You have {limit} guess(es) left')
+        print('Game over')
+        print(f'The random number was {random_number}')
+
+def easy():
+    print("You are to guess a number between 1 and 10, and you have 6 guesses")
+    guess_game(6, 10)
+
+def medium():
+    print("You are to guess a number between 1 and 20, and you have 4 guesses")
+    guess_game(4, 20)
+
+def hard():
+    print("You are to guess a number between 1 and 50, and you have 3 guesses")
+    guess_game(3, 50)
+
+def try_again():
+    again = input('Do you want to play again? Yes/No')
+    if again.upper() == 'YES':
+        welcome()
+    elif again.upper() == 'NO':
+        print('Thanks for playing')
+    else:
+        print('Wrong input')
+        try_again()
+
+def welcome():
